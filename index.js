6128a85f712be6fbe598c720f51ff415b4575d07
Tushar <tm2582001@gmail.com>
2022-07-22 22:00:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const input = document.getElementById('input');
+const defaultText = document.getElementById('default');
+const debounceText = document.getElementById('debounce');
+const throttleText = document.getElementById('throttle');
+
+const updateDebounceText = debounce((text)=>{
+    debounceText.textContent= text;
+});
+
+const updateThrottleText = throttle((text)=>{
+    throttleText.textContent = text;
+});
+
+input.addEventListener('input',e=>{
+    defaultText.textContent = e.target.value;
+    updateDebounceText(e.target.value);
+    updateThrottleText(e.target.value);
+});
+
+
+function debounce(cb,delay = 1000){
+    let timeout;
+    return (...args)=>{
+        clearTimeout(timeout);
+        timeout = setTimeout(()=>{
+            cb(...args)
+        },delay)
+    }
+}
+
+
+ 
+
+function throttle(cb, delay = 1000){
+    let shouldWait = false;
+    let waitingArgs = null;
+
+    timeOutFunc = ()=>{
+
+        if(waitingArgs === null){
+            shouldWait = false;
+        }else{
+            cb(...waitingArgs)
+            waitingArgs = null;
+            setTimeout(timeOutFunc,delay);
+        }
+    
+    }
+
+    return (...args)=>{
+        if (shouldWait){
+            waitingArgs = args;
+            return;
+        } 
+
+        cb(...args);
+        shouldWait = true;
+
+        setTimeout(timeOutFunc,delay);
+    }
+}
